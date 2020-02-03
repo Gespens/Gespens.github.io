a6affbb91c2bf6681f722831e7b65347e5075bca
Gespens <59666248+Gespens@users.noreply.github.com>
2020-02-03 10:32:26 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function obrazek1() {
+	location.href="https://steamcommunity.com/id/stachu258/";
+}
+function obrazek2() {
+	location. href="https://www.facebook.com/krzysztof.kononowicz.58555";
+}
+function obrazek3() {
+	location.href="https://github.com/Gespens";
+}
+function obrazek4() {
+	location.href="https://www.youtube.com/watch?v=fizke4p_QN0";
+}
