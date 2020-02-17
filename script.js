9d6696ed9e01825d7478fb841273d63a47d10d7e
Gespens <59666248+Gespens@users.noreply.github.com>
2020-02-17 15:00:33 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,10 @@
-function obrazek1() {
-	location.href="https://steamcommunity.com/id/stachu258/";
-}
-function obrazek2() {
-	location. href="https://www.facebook.com/krzysztof.kononowicz.58555";
-}
-function obrazek3() {
-	location.href="https://github.com/Gespens";
-}
-function obrazek4() {
-	location.href="https://www.youtube.com/watch?v=fizke4p_QN0";
-}
+
 function menu() {
 	Array.from(document.querySelectorAll(".side-menu")).forEach(el => el.classList.toggle("show"));
 }
 function click1() {
-	location.href="https://www.youtube.com/channel/UCDhSnLzwEdJoWh3te6nLj6Q";
+	location.href="https://www.youtube.com/watch?v=6XiCFMM6c_s";
+}
+function click2() {
+	location.href="https://www.youtube.com/watch?v=2ZIpFytCSVc";
 }
